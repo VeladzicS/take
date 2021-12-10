bf1cd529ecab4f3f797c6e5b4c5880335ace626a
Senahid Veladzic <senahid.veladzic@elatus.net>
2021-12-10 21:06:20 +0100

    .

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -535,5 +535,17 @@
         container:$("#date-wrap")
     })
 
+    $('.testimonials-slick').slick({
+        dots: true,
+        arrows: false,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        lazyLoad: 'ondemand',
+
+
+    });
+
 
 })(jQuery);
